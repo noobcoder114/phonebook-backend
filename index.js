52a57ff817aa7fa8d493d486636ebb1d80175321
ryantohhr <ryantohhongrui@hotmail.com>
2025-05-07 08:21:50 +0800

    Completed 3.15

--- index.js
+++ index.js
@@ -49,9 +49,11 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.filter(person => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            response.status(204).end()
+        })
+        .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response, next) => {
